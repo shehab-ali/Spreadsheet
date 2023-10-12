d784acd321abd6b71526769dd32cb3dfd2196fc1
shehab-ali <shehabali1125@gmail.com>
2023-10-11 20:18:23 -0400

    project phase b

---
+++ src/controller/SpreadsheetController.ts
@@ -0,0 +1,9 @@
+class SpreadsheetController {
+    processInputs(request: any): void {}
+  
+    createModel(inputs: any): SpreadSheet {
+      return new SpreadSheet();
+    }
+  
+    updateUI(model: SpreadSheet): void {}
+}
---
+++ src/model/Cell.ts
@@ -0,0 +1,11 @@
+class Cell {
+    shownValue: any;
+    hiddenValue: any;
+    public Solve(hiddenValue: any, cells: Cell[][]): void {
+        return this.getValue();
+    }
+    public getValue(): any {
+        return this.shownValue;
+    }
+
+}
