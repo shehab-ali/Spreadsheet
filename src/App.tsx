1365e41b6b19c4858b46794d9ec4575bac715163
Jake Writer <jakewriter@Jakes-MacBook-Pro-2.local>
2023-10-29 18:39:22 -0400

    added login page for managing users

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import './App.css';
 import { FileSystemView } from './spreadsheet/view/FileSystemView';
+import LoginScreen from './spreadsheet/view/LoginScreen';
+import { Routes, Route, Navigate, HashRouter, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <FileSystemView />
+    <BrowserRouter> 
+      <Routes> 
+        <Route path="/" element={<Navigate to="Login" />}/> 
+        <Route path="/Login" element={<LoginScreen/>}/> 
+        <Route path="/Dashboard" element={<FileSystemView/>} />
+      </Routes> 
+      </BrowserRouter> 
   );
 }
+
+/*
+        <Route path="Spreadsheets" element={<Assignments/>} />
+        <Route
+          path="Assignments/:sheetId"
+          element={<AssignmentEditor/>}
+        />
+
+*/
 
 
 export default App;
