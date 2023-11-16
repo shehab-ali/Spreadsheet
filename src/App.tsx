3a1df1841fa96ef26e8899f3b1fc0d59be4f0a9b
Alexander Kaufmann <alexkaufmann@Alexanders-MacBook-Pro.local>
2023-11-15 22:11:43 -0500

    added app context for authentication data and navigate() function for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
-import './App.css';
-import { FileSystemView } from './spreadsheet/view/FileSystemView';
-import LoginScreen from './spreadsheet/view/LoginScreen';
-import { Routes, Route, Navigate, HashRouter, BrowserRouter } from "react-router-dom";
-import { SpreadsheetView } from './spreadsheet/view/SpreadSheetView';
+import "./App.css";
+import { FileSystemView } from "./spreadsheet/view/FileSystemView";
+import LoginScreen from "./spreadsheet/view/LoginScreen";
+import {
+  Routes,
+  Route,
+  Navigate,
+  HashRouter,
+  BrowserRouter,
+  useNavigate,
+} from "react-router-dom";
+import { SpreadsheetView } from "./spreadsheet/view/SpreadSheetView";
+import { useContext, createContext, useState, ReactNode } from "react";
+import { AppContext, AppContextType } from "./context";
 
 function App() {
   return (
-    <BrowserRouter> 
-      <Routes> 
-        <Route path="/" element={<Navigate to="/Spreadsheets/2" />}/> 
-        <Route path="/Login" element={<LoginScreen/>}/> 
-        <Route path="/Dashboard" element={<FileSystemView/>} />
-        <Route path="/Spreadsheets/:sheetId" element={<SpreadsheetView/>} />
-      </Routes> 
-      </BrowserRouter> 
+    <BrowserRouter>
+      <AppContextProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/Spreadsheets/2" />} />
+          <Route path="/Login" element={<LoginScreen />} />
+          <Route path="/Dashboard" element={<FileSystemView />} />
+          <Route path="/Spreadsheets/:sheetId" element={<SpreadsheetView />} />
+        </Routes>
+      </AppContextProvider>
+    </BrowserRouter>
   );
 }
+
+interface AppContextProviderProps {
+  children: ReactNode;
+}
+
+// Needed to do all this because you can't use useNavigate outside of a BrowserRouter
+// and you can't just call it in a class component either :/ cause it's a hook
+const AppContextProvider: React.FC<AppContextProviderProps> = ({
+  children,
+}) => {
+  // when logged out, is {}
+  // when logged in, is { id: ... } holds the user_id
+  // for future communication with db/server
+  const [login, setLogin] = useState({});
+  const navigate = useNavigate();
+
+  return (
+    <AppContext.Provider value={{ login, setLogin, navigate }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
 
 /*
         <Route path="Spreadsheets" element={<Assignments/>} />
@@ -25,6 +58,5 @@ function App() {
         />
 
 */
-
 
 export default App;
