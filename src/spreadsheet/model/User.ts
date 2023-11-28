fb5d05cb7b74cdd6cad557bc54cc19274df5049a
smoyer <moyer.s@northeastern.edu>
2023-11-28 13:00:16 -0500

    add/remove/print access

--- src/spreadsheet/model/User.ts
+++ src/spreadsheet/model/User.ts
@@ -12,9 +12,46 @@ export class User {
         this.ID = 0;
     }
   
-    AddReadWrite(spreadsheet: SpreadSheet): void {}
-    RemoveReadWrite(spreadsheet: SpreadSheet): void {}
-    AddReadOnly(spreadsheet: SpreadSheet): void {}
-    RemoveReadOnly(spreadsheet: SpreadSheet): void {}
-}
+
+    AddReadWrite(s: SpreadSheet): void {
+        this.readWrite.push(s)
+    }
+
+    RemoveReadWrite(spreadsheet: SpreadSheet): void {
+      
+        if(this.readWrite.some((sheet) => sheet.id === spreadsheet.id)){
+            this.readWrite = this.readWrite.filter((sheet) => sheet.id !== spreadsheet.id);      
+        } else {
+            throw new Error('User does not have read-write access to this spreadsheet');
+        }
+    }
+    
+    AddReadOnly(s: SpreadSheet): void {
+        this.readOnly.push(s)
+    }
+
+    RemoveReadOnly(spreadsheet: SpreadSheet): void {
+        if(this.readOnly.some((sheet) => sheet.id === spreadsheet.id)){
+            this.readOnly = this.readOnly.filter((sheet) => sheet.id !== spreadsheet.id);      
+        } else {
+            throw new Error('User does not have read-write access to this spreadsheet');
+        }
+    }
+
+    CheckPermissions(): String {
+        const permissions: string[] = [];
+
+        this.readWrite.forEach((sheet) => {
+            permissions.push(`Read-Write permission for ${sheet.name}`);
+        });
+
+        this.readOnly.forEach((sheet) => {
+            permissions.push(`Read-Only permission for ${sheet.name}`);
+        });
+
+        return permissions.length > 0 ? permissions.join('\n') : 'No permissions';
+    }
+
+    }
+
   
