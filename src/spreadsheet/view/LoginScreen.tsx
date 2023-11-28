accc44da24a0350a554ec266fa90f7db6b04827c
Jake Writer <jakewriter@Jakes-MacBook-Pro-2.local>
2023-11-28 14:43:55 -0500

    un-screwing up by switching from using react classes to functions

--- src/spreadsheet/view/LoginScreen.tsx
+++ src/spreadsheet/view/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useContext } from "react";
+import React, { useState, useContext } from "react";
 import "./styles/LoginScreen.css";
 import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { FaEnvelope, FaLock } from "react-icons/fa";
@@ -9,109 +9,108 @@ import { AppContext, AppContextType } from "../../context";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../redux/login";
 
-class LoginScreen extends Component<IProps, IState> {
-  static contextType = AppContext;
+interface IProps {}
 
-  constructor(props: IProps) {
-    super(props);
-    this.state = { authenticated: false, isSigningUp: false };
-  }
+const LoginScreen: React.FC<IProps> = () => {
+  const { setLogin, navigate } = useContext(AppContext) as AppContextType;
+  const dispatch = useDispatch();
+  const [authenticated, setAuthenticated] = useState(false);
+  const [isSigningUp, setIsSigningUp] = useState(false);
 
-  handleSubmit = (event: any) => {
-    const { setLogin, navigate } = this.context as AppContextType;
+  const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    const dispatch = useDispatch();
-    
-    // Not currently secure method, should send server to request instead of using interanl db
 
-    if (this.state.isSigningUp) {
+    if (isSigningUp) {
       // post login data to server db
-      this.setState({...this.state, isSigningUp: false });
+      setIsSigningUp(false);
     } else {
-      const passHash = SHA256(event.target.password.value).toString(enc.Hex);
+      const passHash = SHA256((event.target as any).password.value).toString(
+        enc.Hex
+      );
       const user = db.users.filter(
         (user) =>
-          user.email === event.target.username.value &&
+          user.email === (event.target as any).username.value &&
           user.password_hash === passHash
       );
       if (user.length === 1) {
         console.log("Login Successful");
-  
+
         setLogin({
           id: user[0].id,
         });
 
-        dispatch(loginUser(user[0].id))
+        dispatch(loginUser(user[0].id));
 
         navigate("/Dashboard");
       } else {
         console.log("Login Failed");
       }
-
     }
   };
 
-  render() {
-    return (
-      <div className="login-container">
-        <div className="card m-5 py-5 login-box">
-          <div className="card-header bg-white">
-            <h1>{this.state.isSigningUp ? "Sign Up" : "Login"}</h1>
-          </div>
-          <div className="card-body">
-            <form onSubmit={this.handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="name" className={`${this.state.isSigningUp ? '' : 'hidden'}`}>Full Name</label>
-                <input
-                  type="text"
-                  className={`form-control ${this.state.isSigningUp ? '' : 'hidden'} input-field rounded`}
-                  id="name"
-                  name="name"
-                  required={this.state.isSigningUp}
-                />
-                <label htmlFor="username">Email</label>
-                <input
-                  type="email"
-                  className="form-control input-field rounded"
-                  id="username"
-                  name="username"
-                  required
-                />
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  className="form-control input-field"
-                  id="password"
-                  name="password"
-                  required
-                />
-                <button 
-                type="button"
-                className={`link-button ${this.state.isSigningUp ? 'hidden': ''}`} 
-                onClick={() => {
-                  this.setState({...this.state, isSigningUp: true})
-                }} >
-                  Sign up</button>
-              </div>
-              <button
-                type="submit"
-                className="mt-3 btn-primary btn btn-rounded"
+  return (
+    <div className="login-container">
+      <div className="card m-5 py-5 login-box">
+        <div className="card-header bg-white">
+          <h1>{isSigningUp ? "Sign Up" : "Login"}</h1>
+        </div>
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label
+                htmlFor="name"
+                className={`${isSigningUp ? "" : "hidden"}`}
               >
-                {this.state.isSigningUp ? "Sign Up" : "Login"}
+                Full Name
+              </label>
+              <input
+                type="text"
+                className={`form-control ${
+                  isSigningUp ? "" : "hidden"
+                } input-field rounded`}
+                id="name"
+                name="name"
+                required={isSigningUp}
+              />
+              <label htmlFor="username">Email</label>
+              <input
+                type="email"
+                className="form-control input-field rounded"
+                id="username"
+                name="username"
+                required
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className="form-control input-field"
+                id="password"
+                name="password"
+                required
+              />
+              <button
+                type="button"
+                className={`link-button ${
+                  isSigningUp ? "hidden" : ""
+                }`}
+                onClick={() => {
+                  setIsSigningUp(true);
+                }}
+              >
+                Sign up
               </button>
-            </form>
-          </div>
+            </div>
+            <button
+              type="submit"
+              className="mt-3 btn-primary btn btn-rounded"
+            >
+              {isSigningUp ? "Sign Up" : "Login"}
+            </button>
+          </form>
         </div>
       </div>
-    );
-  }
-}
-
-interface IProps {}
-
-interface IState {
-  authenticated: boolean;
-  isSigningUp: boolean;
-}
+    </div>
+  );
+};
 
 export default LoginScreen;
