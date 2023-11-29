e0494effd6463da3b83f100a4b100a2f9400dcc5
smoyer <moyer.s@northeastern.edu>
2023-11-29 14:29:16 -0500

    unauth nav back

--- src/spreadsheet/view/UnathorizedView.tsx
+++ src/spreadsheet/view/UnathorizedView.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const UnauthorizedView: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/Dashboard');
+  };
+
   return (
     <div>
       <h1>Unauthorized</h1>
       <p>You do not have permission to access this SpreadSheet.</p>
+      <button onClick={handleClick}>Return</button>
     </div>
   );
 };
