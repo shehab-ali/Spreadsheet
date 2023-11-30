b692819f032fca8ddea25abc50da5c3325749a27
smoyer <moyer.s@northeastern.edu>
2023-11-29 22:15:21 -0500

    testing new model

--- src/spreadsheet/model/FormulaFunction.spec.ts
+++ src/spreadsheet/model/FormulaFunction.spec.ts
@@ -10,4 +10,20 @@ describe("should evaluate basic arithmetic function", () => {
     expect(cell.getRawValue()).toBe(expression);
     expect(cell.getDisplayedValue()).toBe('7');
   });
+
+  it("Should evaluate correctly with cell reference", () => {
+    
+    const s = new SpreadSheet("s1", "0", [],5,5);
+    const expression = "A1 + B2";    
+
+    const c1 = s.addCell(0,0,'10');
+    const c2 = s.addCell(0,1,'8');
+    const c3 = s.addCell(1,0,'3');
+    const c4 = s.addCell(1,1,'40');
+    const c5 = s.addCell(1,2, expression);
+ 
+    expect(c5.getDisplayedValue()).toBe('50');
+  });
+
+  
 });  
