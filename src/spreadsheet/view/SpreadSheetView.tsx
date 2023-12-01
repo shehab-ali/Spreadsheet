75c9fe083513a29facdbb1900add47cfb2a1c556
Jake Writer <writer.j@northeastern.edu>
2023-11-30 21:55:03 -0500

    reverted

--- src/spreadsheet/view/SpreadSheetView.tsx
+++ src/spreadsheet/view/SpreadSheetView.tsx
@@ -409,11 +409,11 @@ export const SpreadsheetView = () => {
               {j === 0 && i === 0 ? (
                 ""
               ) : i === 0 ? (
-                String.fromCharCode("A".charCodeAt(0) + ((j - 1) % 26))
-                  .toString()
-                  .repeat(Math.floor((j - 1) / 26) + 1)
+                j
               ) : j === 0 ? (
-                i
+                String.fromCharCode("A".charCodeAt(0) + ((i - 1) % 26))
+                  .toString()
+                  .repeat((i - 1) / 26 + 1)
               ) : (
                 <input
                   type="text"
